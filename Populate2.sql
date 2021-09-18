8c68f74be839a5dda61aa6ca26086778e281df5b
Ocimar <ocimarmota13@gmail.com>
2021-09-18 17:51:40 -0300

    Create Function

--- Populate2.sql
+++ Populate2.sql
@@ -3,8 +3,8 @@ BEGIN
     INSERT
     INTO OKL.OKL_PEDIDO
     VALUES
-    (0, '95693152618', Null, Null),
-    (1, '50539992607', Null, Null),
+    (0, '95693152618', Null, Null), -- VIP
+    (1, '50539992607', Null, Null), -- VIP
     (2, '48564804808', Null, Null),
     (3, '78379403932', Null, Null),
     (4, '78379403932', Null, Null);
@@ -13,7 +13,7 @@ BEGIN
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (1, 0,  1, 10,   5.0,         3);
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (2, 0,  2, 73,   150.0,       3);
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (3, 1,  3, 2,    200.50,      0);
-    INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (4, 1,  4, 0,    600000000.0, 4);
+    INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (4, 1,  4, 1,    600000000.0, 4);
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (5, 3,  0, 270,  5.0,         1); -- Compra mais que o estoque
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (6, 3,  1, 293,  7.0,         4); -- Compra mais que o estoque
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (7, 4,  2, 250,  123.0,       0); -- Compra mais que o estoque
@@ -23,7 +23,7 @@ BEGIN
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (11, 2, 1, 7,    2.99,        0); -- Valor venda menor que preço base
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (12, 2, 2, 34,   80.0,        1); -- Valor venda menor que preço base
     INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (13, 2, 3, 1,    100.0,       3); -- Valor venda menor que preço base
-    INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (14, 3, 4, 0,    999.0,       4); -- Valor venda menor que preço base
+    INSERT INTO OKL.OKL_ITEM_PEDIDO VALUES (14, 3, 4, 1,    999.0,       4); -- Valor venda menor que preço base
 END;
 
 BEGIN EXEC OKL.populate end;
